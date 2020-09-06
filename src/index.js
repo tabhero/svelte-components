4bd826e53e18e1dae401535062095b0ed69dbe8f
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-09-06 14:08:14 +0530

    fix export

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './index.css';
 
 export { default as ActionButton } from './components/ActionButton.svelte';
 export { default as AddTagBar } from './components/AddTagBar.svelte';
+export { default as CarouselNav } from './components/CarouselNav.svelte';
 export { default as Heading } from './components/Heading.svelte';
 export { default as Icon } from './components/Icon.svelte';
 export { default as Info } from './components/Info.svelte';
@@ -12,5 +13,4 @@ export { default as NavLink } from './components/NavLink.svelte';
 export { default as SearchButton } from './components/SearchButton.svelte';
 export { default as Tag } from './components/Tag.svelte';
 export { default as TagGrid } from './components/TagGrid.svelte';
-export { default as TagPages } from './components/TagPages.svelte';
 export { default as TopBar } from './components/TopBar.svelte';

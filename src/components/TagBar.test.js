0b81db874e6679a00f51ec6d60860d1238a811e2
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-10-30 18:10:06 +0530

    test the TagBar input focus

--- src/components/TagBar.test.js
+++ src/components/TagBar.test.js
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/svelte'
+import userEvent from '@testing-library/user-event'
 
 import TagBar from './TagBar'
 
@@ -8,4 +9,14 @@ test('has an input element with the proper placeholder', () => {
   const { getByPlaceholderText } = render(TagBar, {})
 
   expect(getByPlaceholderText(placeholder)).toBeInTheDocument()
+})
+
+test('has an input element that gets focused on pressing TAB', () => {
+  const placeholder = 'Search from your tag library or create a new tag!'
+
+  const { getByPlaceholderText } = render(TagBar, {})
+
+  userEvent.tab()
+
+  expect(getByPlaceholderText(placeholder)).toHaveFocus()
 })

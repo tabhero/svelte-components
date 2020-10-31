c92ef567a2743c63d28c97d64ff2056c2bcbc71f
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-10-31 18:21:50 +0530

    add tests for exact match case

--- src/components/TagBar.test.js
+++ src/components/TagBar.test.js
@@ -46,6 +46,20 @@ test('focuses on the prompt on pressing down arrow when no exact match', async (
   expect(getByText('+Create New Tag and Add').closest('li')).toHaveFocus()
 })
 
+test('focuses on the matched suggestion on pressing down arrow when exact match present', async () => {
+  const { getByTestId, getByText } = render(TagBar, {
+    input: 'one',
+    suggestions: [
+      { id: 'xyz', added: false, name: 'one' }
+    ]
+  })
+  const target = getByTestId('container')
+
+  await fireArrowDown(target)
+
+  expect(getByText('one').closest('li')).toHaveFocus()
+})
+
 test('focuses on the prompt on down arrow when some input but no suggestions present', async () => {
   const { getByTestId, getByText } = render(TagBar, {
     input: 'something',
@@ -125,6 +139,20 @@ test('focuses on the last suggestion on pressing up arrow when no exact match', 
   await fireArrowUp(target)
 
   expect(getByText('two').closest('li')).toHaveFocus()
+})
+
+test('focuses on the matched suggestion on pressing up arrow when exact match present', async () => {
+  const { getByTestId, getByText } = render(TagBar, {
+    input: 'one',
+    suggestions: [
+      { id: 'xyz', added: false, name: 'one' }
+    ]
+  })
+  const target = getByTestId('container')
+
+  await fireArrowUp(target)
+
+  expect(getByText('one').closest('li')).toHaveFocus()
 })
 
 test('focuses on the second last suggestion on pressing two up arrows when no exact match', async () => {

6ede58205f8a37c1c6808d4375bd66d35bbc1094
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-10-31 18:27:05 +0530

    refactor tests

--- src/components/TagBar.test.js
+++ src/components/TagBar.test.js
@@ -72,18 +72,6 @@ test('focuses on the prompt on down arrow when some input but no suggestions pre
   expect(getByText('+Create New Tag and Add').closest('li')).toHaveFocus()
 })
 
-test('focuses on the prompt on up arrow when some input but no suggestions present', async () => {
-  const { getByTestId, getByText } = render(TagBar, {
-    input: 'something',
-    suggestions: []
-  })
-  const target = getByTestId('container')
-
-  await fireArrowUp(target)
-
-  expect(getByText('+Create New Tag and Add').closest('li')).toHaveFocus()
-})
-
 test('focuses on the first suggestion on pressing two down arrows when no exact match', async () => {
   const { getByTestId, getByText } = render(TagBar, {
     input: 'something',
@@ -153,6 +141,18 @@ test('focuses on the matched suggestion on pressing up arrow when exact match pr
   await fireArrowUp(target)
 
   expect(getByText('one').closest('li')).toHaveFocus()
+})
+
+test('focuses on the prompt on up arrow when some input but no suggestions present', async () => {
+  const { getByTestId, getByText } = render(TagBar, {
+    input: 'something',
+    suggestions: []
+  })
+  const target = getByTestId('container')
+
+  await fireArrowUp(target)
+
+  expect(getByText('+Create New Tag and Add').closest('li')).toHaveFocus()
 })
 
 test('focuses on the second last suggestion on pressing two up arrows when no exact match', async () => {
